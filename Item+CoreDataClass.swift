860dcea229260b7391b70f71a60073af549928bd
Peter Leung <babyghost@gmail.com>
2016-11-16 19:37:10 +0000

    Codegen need to set manual to manage class by myself

---
+++ Item+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Item+CoreDataClass.swift
+//  iOS 10 Core Data
+//
+//  Created by Peter Leung on 16/11/2016.
+//  Copyright © 2016 winandmac Media. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
---
+++ Item+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  Item+CoreDataProperties.swift
+//  iOS 10 Core Data
+//
+//  Created by Peter Leung on 16/11/2016.
+//  Copyright © 2016 winandmac Media. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
+extension Item {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Item> {
+        return NSFetchRequest<Item>(entityName: "Item");
+    }
+
+    @NSManaged public var created: NSDate?
+    @NSManaged public var details: String?
+    @NSManaged public var price: Double
+    @NSManaged public var title: String?
+    @NSManaged public var toImage: Image?
+    @NSManaged public var toItemType: ItemType?
+    @NSManaged public var toStore: Store?
+
+}
