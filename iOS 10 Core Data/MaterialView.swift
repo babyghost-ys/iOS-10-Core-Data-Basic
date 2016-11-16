2161dd287606394c70867af1624f0c8d1c71b8f3
Peter Leung <babyghost@gmail.com>
2016-11-16 19:59:48 +0000

    General UI Design Set.

---
+++ iOS 10 Core Data/MaterialView.swift
@@ -0,0 +1,42 @@
+//
+//  MaterialView.swift
+//  iOS 10 Core Data
+//
+//  Created by Peter Leung on 16/11/2016.
+//  Copyright © 2016 winandmac Media. All rights reserved.
+//
+
+import UIKit
+
+private var materialKey = false
+
+extension UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    @IBInspectable var materialDesign: Bool {
+        get {
+            return materialKey
+        }
+        
+        set {
+            materialKey = newValue
+            
+            if materialKey {
+                self.layer.masksToBounds = false
+                self.layer.cornerRadius = 3.0
+                self.layer.opacity = 0.8
+                self.layer.shadowRadius = 3.0
+                self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+                self.layer.shadowColor = UIColor(red: 157/255, green: 157/255, blue: 157/255, alpha: 1.0).cgColor
+                
+            }
+        }
+        
+    }
+}
